f7d93eba7623b77c3358ccbd1a23f6ec9c073d46
Sarin Linx <sarinlinx@gmail.com>
2019-01-15 21:34:25 -0500

    Refined all examples

--- projects/note-taking-app/final-notes-app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#create-note').addEventListener('click', function (e) {
-    e.target.textContent = 'The button was clicked'
-})
-
-document.querySelector('#remove-all').addEventListener('click', function () {
-    document.querySelectorAll('.note').forEach(function( note) {
-        note.remove()
-    })
-})
