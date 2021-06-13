3e9a6a5fc2b5d3a4e3f7e9cbea954c85ece43a76
sarinlinx <sarinlinx@gmail.com>
2021-06-12 23:28:41 -0400

    filter examples

--- projects/todo-app/todo-app-04.js
+++ projects/todo-app/todo-app-04.js
@@ -15,16 +15,19 @@ const todos = [{
     completed: true
 }]
 
+
+
+
 const filters = {
     searchText: ''
 }
 
-const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
+const renderTodos = function(todos, filters) {
+    const filteredTodos = todos.filter(function(todo) {
         return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
-    const incompleteTodos = filteredTodos.filter(function (todo) {
+    const incompleteTodos = filteredTodos.filter(function(todo) {
         return !todo.completed
     })
 
@@ -34,7 +37,7 @@ const renderTodos = function (todos, filters) {
     summary.textContent = `You have ${incompleteTodos.length} todos left`
     document.querySelector('#todos').appendChild(summary)
 
-    filteredTodos.forEach(function (todo) {
+    filteredTodos.forEach(function(todo) {
         const p = document.createElement('p')
         p.textContent = todo.text
         document.querySelector('#todos').appendChild(p)
@@ -43,17 +46,30 @@ const renderTodos = function (todos, filters) {
 
 renderTodos(todos, filters)
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+document.querySelector('#search-text').addEventListener('input', function(e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
 
-document.querySelector('#new-todo').addEventListener('submit', function (e) {
+
+
+
+
+
+
+// target element with ID of #new-todo and listen for 'submit' event
+document.querySelector('#new-todo').addEventListener('submit', function(e) {
+    // prevent default submit action
     e.preventDefault()
+        // add to Array with push()
     todos.push({
-        text: e.target.elements.text.value,
-        completed: false
-    })
+            // text and completed are properties of the original todos Array
+            // set the 'text' property to the input fields text
+            text: e.target.elements.text.value,
+            completed: false
+        })
+        // call function
     renderTodos(todos, filters)
+        // clear input value
     e.target.elements.text.value = ''
 })
